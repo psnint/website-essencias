4c7a07dd4601b947ec7ef7714ecd78201b291442
Gonçalo Fialho <goncalo.fialho@mercedes-benz.io>
2019-10-10 22:10:29 +0100

    added new builded FE folder

---
+++ website_essencias_pwa/essencias_catalogo/frontend/dist/precache-manifest.9c64d301a156d00c51dfcfa41f6120fa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b68fdf6052f7fd141dfa47f489fe7bbc",
+    "url": "/img/uk-flag.b68fdf60.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d83d49081303d7274083",
+    "url": "/js/chunk-vendors.0284c43b.js"
+  },
+  {
+    "revision": "b21d3d4db625c43111ac",
+    "url": "/js/app.730507b9.js"
+  },
+  {
+    "revision": "297c31388668e58efebc",
+    "url": "/js/about.df14970a.js"
+  },
+  {
+    "revision": "4564e64ce8294c348e9c4fafe1f4181e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32f8be86c4d0e908fb8ffcf8979432d8",
+    "url": "/img/pt-flag.32f8be86.png"
+  },
+  {
+    "revision": "6c8f247e406c696424c163ef653f23b4",
+    "url": "/img/logo-essencia.6c8f247e.png"
+  },
+  {
+    "revision": "c7461e213a0a2cc76ad19539e27c2c18",
+    "url": "/img/background.c7461e21.jpg"
+  },
+  {
+    "revision": "19cb1809d619ea8d455233b2ee3a660d",
+    "url": "/img/Page-9-2.19cb1809.png"
+  },
+  {
+    "revision": "b21d3d4db625c43111ac",
+    "url": "/css/app.bda5c251.css"
+  }
+];
