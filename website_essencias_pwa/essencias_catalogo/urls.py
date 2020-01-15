7f5f502c10ec5d83a1ba396f3caa718d6b1a2843
Pedro Duarte <pedro_migueld@hotmail.com>
2020-01-15 07:54:38 -0500

    Changed urls.py in order to serve media files when DEBUG = False

--- website_essencias_pwa/essencias_catalogo/urls.py
+++ website_essencias_pwa/essencias_catalogo/urls.py
@@ -1,6 +1,8 @@
 from django.conf import settings  # new
 from django.urls import path, include  # new
 from django.conf.urls.static import static  # new
+from django.conf.urls import url
+from django.views.static import serve
 
 from . import views
 
@@ -10,8 +12,9 @@ urlpatterns = [
     path('products', views.products, name='products'),
     path('collection/', views.collection, name='collection'),
     path('downloadCollection', views.download_collection, name='download_collection')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
-
-# if settings.DEBUG:  # new
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
+]
